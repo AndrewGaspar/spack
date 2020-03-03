a03687e4f8ae6e9356f73bd43d6a29f03f25ca84
Andrew Gaspar <agaspar@lanl.gov>
2020-03-03 07:37:06 -0700

    WIP: Additional improvements to rust-bootstrap-1-29

--- var/spack/repos/builtin/packages/rust-bootstrap-1-29/package.py
+++ var/spack/repos/builtin/packages/rust-bootstrap-1-29/package.py
@@ -27,16 +27,30 @@ class RustBootstrap129(Package):
     "Builds Rust 1.29"
 
     homepage = "https://www.rust-lang.org/"
-    url      = "https://static.rust-lang.org/dist/rustc-1.29.0-src.tar.gz"
+    url      = "https://static.rust-lang.org/dist/rustc-1.29.2-src.tar.gz"
 
     maintainers = ['AndrewGaspar']
 
-    # version('1.2.4')
+    version('1.29.2', sha256='5088e796aa2e47478cdf41e7243fc5443fafab0a7c70a11423e57c80c04167c9')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
+    depends_on('cmake', type='build')
+    depends_on('binutils', type='build')
+    depends_on('python@:2.8', type='build')
+    depends_on('mrustc +mrustc +minicargo', type='build')
+    depends_on('openssl')
 
     def install(self, spec, prefix):
-        # FIXME: Unknown build system
-        make()
-        make('install')
+        make(
+            '-f', join_path(spec['mrustc'].prefix.share, 'minicargo.mk'),
+            'RUSTCSRC=./',
+            'RUSTC_VERSION=1.29.0',
+            'MRUSTC=%s' % join_path(spec['mrustc'].prefix.bin, 'mrustc'),
+            'MINICARGO=%s' % join_path(spec['mrustc'].prefix.tools.bin, 'minicargo'),
+            'output/rustc',
+            'output/cargo'
+        )
+
+        # FIXME: This needs to self-bootstrap to a Rust 1.29 compiler built with 1.29
+        mkdirp(prefix.bin)
+        install('output/rustc', prefix.bin)
+        install('output/cargo', prefix.bin)
