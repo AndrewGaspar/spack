f48fefc48444d83fc62638e8d4b1a8405cc4b1ef
Andrew Gaspar <agaspar@lanl.gov>
2020-03-07 18:32:28 -0700

    mrustc bootstrap overhaul

--- var/spack/repos/builtin/packages/rust-bootstrap-1-29-mrustc/package.py
+++ var/spack/repos/builtin/packages/rust-bootstrap-1-29-mrustc/package.py
@@ -24,70 +24,46 @@ class RustBootstrap129Mrustc(Package):
         placement='rustc-1.29.0-src'
     )
 
+    depends_on('cmake', type='build')
+    depends_on('binutils', type='build')
+    depends_on('python@:2.8', type='build')
+    depends_on('openssl')
+    depends_on('libssh2')
+    depends_on('libgit2')
+
+    provides('rust-can-bootstrap-1-30')
+
+    def patch(self):
+        # raise InstallError('cwd: ' + os.getcwd())
+        # Apply the source patch
+        patch = which("patch", required=True)
+        patch(
+            '-s',
+            '-p0',
+            '-i', '../rustc-1.29.0-src.patch',
+            '-d', './rustc-1.29.0-src')
+
     def install(self, spec, prefix):
-        ()
+        make(
+            '-f', 'minicargo.mk',
+            'output/rustc',
+            'output/cargo',
+            extra_env={
+                # vendored libgit2 wasn't correctly building (couldn't find the
+                # vendored libssh2), so let's just have spack build it
+                'LIBSSH2_SYS_USE_PKG_CONFIG': '1',
+                'LIBGIT2_SYS_USE_PKG_CONFIG': '1'
+            }
+        )
 
-    # depends_on('cmake', type='build')
-    # depends_on('binutils', type='build')
-    # depends_on('python@:2.8', type='build')
-    # depends_on('mrustc +mrustc +minicargo', type='build')
-    # depends_on('openssl')
-    # depends_on('libssh2')
-    # depends_on('libgit2')
-
-    # provides('rust-can-bootstrap-1-30')
-
-    # def install(self, spec, prefix):
-    #     # Emplace the lib/librustc_macro re-implementation from mrustc
-    #     mkdirp('lib')
-    #     install_tree(spec['mrustc'].prefix.lib, 'lib')
-
-    #     # Apply the source patch
-    #     patch = which("patch", required=True)
-    #     patch(
-    #         '-s',
-    #         '-p0',
-    #         '-i', join_path(spec['mrustc'].prefix, 'rustc-1.29.0-src.patch'))
-
-    #     make(
-    #         '-f', join_path(spec['mrustc'].prefix.share, 'minicargo.mk'),
-    #         'RUSTCSRC=./',
-    #         'RUSTC_VERSION=1.29.0',
-    #         'MRUSTC=%s' % join_path(spec['mrustc'].prefix.bin, 'mrustc'),
-    #         'MINICARGO=%s'
-    #         % join_path(spec['mrustc'].prefix.tools.bin, 'minicargo'),
-    #         'OVERRIDE_DIR=%s'
-    #         % join_path(
-    #             spec['mrustc'].prefix,
-    #             'script-overrides/stable-1.29.0-linux/'),
-    #         'output/rustc',
-    #         'output/cargo',
-    #         extra_env={
-    #             # vendored libgit2 wasn't correctly building (couldn't find the
-    #             # vendored libssh2), so let's just have spack build it
-    #             'LIBSSH2_SYS_USE_PKG_CONFIG': '1',
-    #             'LIBGIT2_SYS_USE_PKG_CONFIG': '1'
-    #         }
-    #     )
-
-    #     install_tree(spec['mrustc'].prefix.run_rustc, 'run_rustc')
-
-    #     # pushd
-    #     cwd = os.getcwd()
-    #     os.chdir('run_rustc')
-
-    #     try:
-    #         make(
-    #             'RUST_SRC=..',
-    #             'RUSTC_VERSION=1.29.0',
-    #             'PREFIX={}'.format(prefix),
-    #             extra_env={
-    #                 # vendored libgit2 wasn't correctly building (couldn't find
-    #                 # the vendored libssh2), so let's just have spack build it
-    #                 'LIBSSH2_SYS_USE_PKG_CONFIG': '1',
-    #                 'LIBGIT2_SYS_USE_PKG_CONFIG': '1'
-    #             }
-    #         )
-    #     finally:
-    #         #popd
-    #         os.chdir(cwd)
+        make(
+            '-C', 'run_rustc',
+            extra_env={
+                # vendored libgit2 wasn't correctly building (couldn't find
+                # the vendored libssh2), so let's just have spack build it
+                'LIBSSH2_SYS_USE_PKG_CONFIG': '1',
+                'LIBGIT2_SYS_USE_PKG_CONFIG': '1'
+            }
+        )
+        
+        install_tree('run_rustc/output/prefix', prefix)
